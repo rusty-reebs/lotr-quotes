36a8be6c67f75fd725cda0042e79c84c3bf58fdf
rusty-reebs <rusty_reebs@tutanota.com>
2021-09-20 16:55:51 +0400

    fully functional

--- script.js
+++ script.js
@@ -2,99 +2,36 @@ const headers = {
   Accept: "application/json",
   Authorization: "Bearer QekNlgXMfXmuGPWF_nSV",
 };
-//! The code below works
-// let quote;
-// fetch("https://the-one-api.dev/v2/quote", {
-//   headers: headers,
-// })
-//   .then(function (response) {
-//     // console.log(response.json());
-//     return response.json();
-//   })
-//   .then(function (response) {
-//     // quote = response.docs[0].dialog;
-//     quote = response;
-//     console.log(quote.docs[0].dialog);
-//   });
-// setTimeout(function () {
-//   fetch("https://the-one-api.dev/v2/character?_id=" + quote.docs[0].character, {
-//     headers: headers,
-//   })
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (response) {
-//       console.log(response.docs[0].name);
-//     });
-// }, 1000);
 
-// let gandalf;
-// let quotes;
-// fetch("https://the-one-api.dev/v2/character?name=Gandalf", {
-//   headers: headers,
-// })
-//   .then((response) => {
-//     // console.log(response.json());
-//     return response.json();
-//   })
-//   .then((response) => {
-//     gandalf = response;
-//   });
-// setTimeout(function () {
-//   fetch(
-//     "https://the-one-api.dev/v2/character/" + gandalf.docs[0]._id + "/quote",
-//     {
-//       headers: headers,
-//     }
-//   )
-//     .then((response) => {
-//       // console.log(response.json());
-//       return response.json();
-//     })
-//     .then((response) => {
-//       quotes = response;
-//     });
-// }, 1000);
-
-// function to fetch random quote by character
-
-let characterObj;
-const getCharacter = (character) => {
-  fetch("https://the-one-api.dev/v2/character?name=" + character, {
-    headers: headers,
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((response) => {
-      characterObj = response;
-      //   console.log(characterObj);
-    });
-};
-
-let quotes;
-const getQuotes = () => {
-  fetch(
-    "https://the-one-api.dev/v2/character/" +
-      characterObj.docs[0]._id +
-      "/quote",
+let charId;
+async function getCharacter(character) {
+  const response = await fetch(
+    "https://the-one-api.dev/v2/character?name=" + character,
     {
       headers: headers,
     }
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then((response) => {
-      // console.log(response.json());
-      quotes = response;
-    });
-};
+  );
+  const charData = await response.json();
+  charId = charData.docs[0]._id;
+}
 
+let quotes;
+async function getQuotes() {
+  const response = await fetch(
+    "https://the-one-api.dev/v2/character/" + charId + "/quote",
+    {
+      headers: headers,
+    }
+  );
+  const quoteData = await response.json();
+  quotes = quoteData;
+}
+
+let singlequote;
 const showthequote = () => {
-  let singlequote =
-    quotes.docs[Math.floor(Math.random() * quotes?.docs?.length)];
+  singlequote = quotes.docs[Math.floor(Math.random() * quotes?.docs?.length)];
   console.log(singlequote.dialog);
+  characterQuote.innerHTML = singlequote.dialog;
 };
 
 const body = document.querySelector("body");
@@ -137,48 +74,32 @@ const quoteDiv = document.createElement("div");
 quoteDiv.classList.add("quotediv");
 const characterQuote = document.createElement("div");
 characterQuote.classList.add("quote");
-characterQuote.innerHTML = "Run, you fools!";
 
 mainContainer.appendChild(spacer);
 mainContainer.appendChild(quoteDiv);
 quoteDiv.appendChild(characterQuote);
 
+async function getQuote(character) {
+  await getCharacter(character);
+  await getQuotes();
+  showthequote();
+}
 gandalfButton.addEventListener("click", () => {
   console.log("you clicked Gandalf!");
-  getCharacter("Gandalf");
-  setTimeout(function () {
-    console.log("waiting");
-    getQuotes();
-  }, 2000);
-  //   setTimeout(showthequote, 2000);
+  getQuote("Gandalf");
 });
 
 gollumButton.addEventListener("click", () => {
   console.log("you clicked Gollum!");
-  getCharacter("Gollum");
-  setTimeout(function () {
-    console.log("waiting");
-    getQuotes();
-  }, 2000);
-  //   setTimeout(showthequote, 2000);
+  getQuote("Gollum");
 });
 
 gimliButton.addEventListener("click", () => {
   console.log("you clicked Gimli!");
-  getCharacter("Gimli");
-  setTimeout(function () {
-    console.log("waiting");
-    getQuotes();
-  }, 2000);
-  //   setTimeout(showthequote, 2000);
+  getQuote("Gimli");
 });
 
 frodoButton.addEventListener("click", () => {
   console.log("you clicked Frodo!");
-  getCharacter("Frodo");
-  setTimeout(function () {
-    console.log("waiting");
-    getQuotes();
-  }, 2000);
-  //   setTimeout(showthequote, 2000);
+  getQuote("Frodo Baggins");
 });
